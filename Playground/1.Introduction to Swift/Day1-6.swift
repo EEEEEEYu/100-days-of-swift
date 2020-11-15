aa1b71b3c6c3e533c20feb6605c032b4f351b3c9
ElvisYu <elvisyuovo@gmail.com>
2020-11-15 23:42:23 +0800

    initial commit

---
+++ Playground/1.Introduction to Swift/Day1-6.swift
@@ -0,0 +1,19 @@
+//
+//  Day1-6.swift
+//  Playground
+//
+//  Created by 余浩文 on 2020/11/15.
+//
+
+import Foundation
+
+
+/***************  Day1  ***************/
+
+func Day1(){
+    var str = "Hello, playground"
+    str = "A new string"
+    print(str)
+}
+
+/***************  Day1  ***************/
