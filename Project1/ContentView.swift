fac569b90139a43fdcf0cc42f2610615f9d30c41
ElvisYu <elvisyuovo@gmail.com>
2020-12-22 21:25:04 +0800

    add project1

--- Project1/ContentView.swift
+++ Project1/ContentView.swift
@@ -9,8 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                // 一个Form里最多展示10条数据，再多需要分组，使用Group组件可以分组
+                Group {
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+                Section {
+                    Text("Hello World")
+                }
+                // Section组件会自动切分
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+            
+            }
+        }
+        .navigationBarTitle(Text("SwiftUI"))
     }
 }
 
