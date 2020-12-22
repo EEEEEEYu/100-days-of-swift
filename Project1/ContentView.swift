926abfe03b8ff2c144fa7442b54d782e78c72e77
ElvisYu <elvisyuovo@gmail.com>
2020-12-22 19:57:59 +0800

    Initial Commit

---
+++ Project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1
+//
+//  Created by 余浩文 on 2020/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by 余浩文 on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
